7a44520ff5e14fbba711fda2cf055680ab6ed693
SoroushGReza <mycodeschool22@outlook.com>
2024-08-28 18:49:29 +0000

    - Added `NavBar` component to be conditionally rendered based on the current route. - Created a new `AppContent` component to handle conditional rendering and routing. - Used `useLocation` hook to determine the current path and render `NavBar` only when the path is not `'/'`. - Moved `Routes` into the new `AppContent` component. - Updated the `App` component to include `AppContent` instead of directly rendering `Routes`.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,30 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
+import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 import Services from "./pages/Services";
 import About from "./pages/About";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+function AppContent() {
+  const location = useLocation();
+
   return (
-    <Router>
+    <>
+      {/* Rendera NavBar om den aktuella sökvägen inte är '/' */}
+      {location.pathname !== "/" && <NavBar />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/services" element={<Services />} />
         <Route path="/about" element={<About />} />
       </Routes>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <AppContent />
     </Router>
   );
 }
