42cce6c07e3b737eb1da83530729b5400f0d75b1
SoroushGReza <mycodeschool22@outlook.com>
2024-08-28 07:41:45 +0000

    Imported 'About' from pages and added Route for it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Services from "./pages/Services";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import About from "./pages/About";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/services" element={<Services />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
