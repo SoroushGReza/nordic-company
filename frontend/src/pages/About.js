e47a15d9772831eb0d68fde88ecd9c39b07c34b0
SoroushGReza <mycodeschool22@outlook.com>
2024-08-28 07:45:44 +0000

    Created About.js to implement frontend logic for the 'About us' page.

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import styles from "../styles/About.module.css";
+
+const About = () => {
+  return 
+};
+
+export default About;
