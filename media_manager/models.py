f6d936da3f8787c0d1b48f977ab287fff7999efe
SoroushGReza <mycodeschool22@outlook.com>
2024-08-30 11:19:07 +0000

    - Updated the `uploaded_by` field to reference the custom user model by using `settings.AUTH_USER_MODEL` instead of the direct `User` model reference, allowing for better flexibility and compatibility with custom user models.

--- media_manager/models.py
+++ media_manager/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 from cloudinary_storage.storage import MediaCloudinaryStorage
 
 
@@ -7,7 +7,9 @@ class Media(models.Model):
     title = models.CharField(max_length=100)
     file = models.FileField(storage=MediaCloudinaryStorage())
     uploaded_at = models.DateTimeField(auto_now_add=True)
-    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    uploaded_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return self.title
