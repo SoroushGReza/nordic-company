03a919bc6f8d105466728c0e9bf34963b86b12f5
SoroushGReza <mycodeschool22@outlook.com>
2024-08-31 08:36:28 +0000

    - Removed the `password2` field from `UserRegistrationSerializer`. - Updated the `password` field in `UserRegistrationSerializer` to remove validation constraints. - Simplified the `create` method in `UserRegistrationSerializer` by using `create_user` instead of `create`.

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,39 +1,24 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from django.contrib.auth.password_validation import validate_password
 
 CustomUser = get_user_model()
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(
-        write_only=True, required=True, validators=[validate_password]
-    )
-    password2 = serializers.CharField(write_only=True, required=True)
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = CustomUser
-        fields = (
-            "email", "name", "surname", "phone_number",
-            "password", "password2"
-        )
-
-    def validate(self, attrs):
-        if attrs["password"] != attrs["password2"]:
-            raise serializers.ValidationError(
-                {"password": "Password fields didn't match."}
-            )
-        return attrs
+        fields = ("email", "password", "name", "surname", "phone_number")
 
     def create(self, validated_data):
-        user = CustomUser.objects.create(
+        user = CustomUser.objects.create_user(
             email=validated_data["email"],
-            name=validated_data["name"],
-            surname=validated_data["surname"],
-            phone_number=validated_data["phone_number"],
+            password=validated_data["password"],
+            name=validated_data.get("name"),
+            surname=validated_data.get("surname"),
+            phone_number=validated_data.get("phone_number"),
         )
-        user.set_password(validated_data["password"])
-        user.save()
         return user
 
 
