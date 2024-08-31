5b9e50f7238f6a1a3c9cb73799c3afa92ce6cdda
SoroushGReza <mycodeschool22@outlook.com>
2024-08-31 08:43:32 +0000

    - Configured Axios with a base URL for API requests and set default headers for `Content-Type` and credentials. - Integrated `js-cookie` to retrieve the `access_token` from cookies and set the `Authorization` header if the token exists. - Created `axiosReq` and `axiosRes` instances using Axios for making API requests with the shared configuration.

---
+++ frontend/src/api/axiosDefaults.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+axios.defaults.baseURL =
+  "https://8000-soroushgrez-tinyswebsit-hmdy1e3e5ct.ws-us115.gitpod.io/api";
+axios.defaults.headers.post["Content-Type"] = "application/json";
+axios.defaults.withCredentials = true;
+
+const token = Cookies.get("access_token");
+
+if (token) {
+  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+}
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
