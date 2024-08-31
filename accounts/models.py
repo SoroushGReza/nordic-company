90a09ecd4574f31e89c9f9601e545e12e9080ff3
SoroushGReza <mycodeschool22@outlook.com>
2024-08-31 08:33:53 +0000

    - Added a `username` field to the `CustomUser` model to allow users to have an optional username. - Updated the `create_user` method in `CustomUserManager` to accept a `username` parameter. - Modified the `create_superuser` method in `CustomUserManager` to include the `username` parameter when creating a superuser. - Updated the `REQUIRED_FIELDS` in `CustomUser` to include `username` as a required field for superusers. - Set the `username` field in `CustomUser` to be nullable by adding `null=True`.

--- accounts/models.py
+++ accounts/models.py
@@ -8,23 +8,26 @@ from django.utils import timezone
 
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, email, username=None, password=None, **extra_fields):
         if not email:
             raise ValueError("The Email field must be set")
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=email, username=username, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None, **extra_fields):
+    def create_superuser(
+        self, email, username=None, password=None, **extra_fields
+    ):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
 
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(email, username, password, **extra_fields)
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=150, null=True)
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=30)
     surname = models.CharField(max_length=30)
@@ -47,7 +50,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     objects = CustomUserManager()
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["name", "surname", "phone_number"]
+    REQUIRED_FIELDS = ["username", "name", "surname", "phone_number"]
 
     def __str__(self):
         return self.email
