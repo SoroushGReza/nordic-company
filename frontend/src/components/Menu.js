ba79c6221ef2d46abe166ffede56428a0e551349
SoroushGReza <soroush_developer@hotmail.com>
2024-09-01 17:56:01 +0300

    - Added `useNavigate` from `react-router-dom` for programmatic navigation. - Implemented `isAuthenticated` function to check user authentication status using a token from `localStorage`. - Updated "Book appointment" button to conditionally navigate to the booking page if authenticated, otherwise redirect to the login page. - Added a `handleLogout` function to log out the user by removing tokens from `localStorage` and reloading the page. - Modified the menu to conditionally render "Pre appointment" and "Aftercare tips" buttons only when the user is authenticated. - Added conditional rendering for "Login" and "Register" buttons when the user is not authenticated. - Added a "Logout" button that appears when the user is authenticated. - Removed the "Aftercare tips" button when the user is not authenticated.

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -3,51 +3,108 @@ import { Button, Container } from "react-bootstrap";
 import styles from "../styles/Menu.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Menu = () => {
-  return (
-    <Container className={styles.menu}>
-      <Link to="/services" className={styles.link}>
-        <Button variant="outline-dark" className={styles.menuButtonTop}>
-          SERVICES
-        </Button>
-      </Link>
-      <Link to="/about" className={styles.link}>
-        <Button variant="outline-dark" className={styles.menuButton}>
-          About us
-        </Button>
-      </Link>
-      <Link to="/aftercare-tips" className={styles.link}>
-        <Button variant="outline-dark" className={styles.menuButton}>
-          Aftercare tips
-        </Button>
-      </Link>
-      <Link to="/shop" className={styles.link}>
-        <Button variant="outline-dark" className={styles.menuButton}>
-          Shop products
-        </Button>
-      </Link>
-      <Link to="/pre-appointment-info" className={styles.link}>
-        <Button variant="outline-dark" className={styles.menuButton}>
-          Pre appointment
-        </Button>
-      </Link>
-      <Link to="/book" className={styles.link}>
-        <Button variant="outline-dark" className={styles.menuButton}>
-          Book appointment
-        </Button>
-      </Link>
-      <a
-        href="https://www.instagram.com/facebykristine/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className={styles.link}
-      >
-        <FontAwesomeIcon className={styles.instagram} icon={faInstagram} />
-      </a>
-    </Container>
-  );
+    const navigate = useNavigate();
+
+    const isAuthenticated = () => {
+        const token = localStorage.getItem("access");
+        return token !== null;
+    };
+
+    const handleBookAppointmentClick = () => {
+        if (isAuthenticated()) {
+            navigate("/book");
+        } else {
+            navigate("/login");
+        }
+    };
+
+    const handleLogout = () => {
+        localStorage.removeItem("access");
+        localStorage.removeItem("refresh");
+        window.location.reload(); // Ladda om sidan för att tillämpa ändringarna
+    };
+
+    return (
+        <Container className={styles.menu}>
+            <Link to="/services" className={styles.link}>
+                <Button variant="outline-dark" className={styles.menuButtonTop}>
+                    SERVICES
+                </Button>
+            </Link>
+            <Link to="/about" className={styles.link}>
+                <Button variant="outline-dark" className={styles.menuButton}>
+                    About us
+                </Button>
+            </Link>
+            <Link to="/shop" className={styles.link}>
+                <Button variant="outline-dark" className={styles.menuButton}>
+                    Shop
+                </Button>
+            </Link>
+
+            {/* Book Appointment Button */}
+            <Button
+                variant="outline-dark"
+                className={styles.menuButton}
+                onClick={handleBookAppointmentClick}
+            >
+                Book appointment
+            </Button>
+            {isAuthenticated() && (
+                <>
+                    <Link to="/pre-appointment-info" className={styles.link}>
+                        <Button variant="outline-dark" className={styles.menuButton}>
+                            Pre appointment
+                        </Button>
+                    </Link>
+                    <Link to="/aftercare-tips" className={styles.link}>
+                        <Button variant="outline-dark" className={styles.menuButton}>
+                            Aftercare tips
+                        </Button>
+                    </Link>
+                </>
+            )}
+
+
+            {!isAuthenticated() && (
+                <>
+                    <Link to="/login" className={styles.link}>
+                        <Button variant="outline-dark" className={styles.menuButton}>
+                            Login
+                        </Button>
+                    </Link>
+                    <Link to="/register" className={styles.link}>
+                        <Button variant="outline-dark" className={styles.menuButton}>
+                            Register
+                        </Button>
+                    </Link>
+                </>
+            )}
+
+            {isAuthenticated() && (
+                <Button
+                    variant="outline-dark"
+                    className={styles.menuButton}
+                    onClick={handleLogout}
+                >
+                    Logout
+                </Button>
+            )}
+
+            {/* Instagram Link */}
+            <a
+                href="https://www.instagram.com/facebykristine/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.link}
+            >
+                <FontAwesomeIcon className={styles.instagram} icon={faInstagram} />
+            </a>
+        </Container>
+    );
 };
 
 export default Menu;
