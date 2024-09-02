45cd104d8c8b04c8d3aa3cf9ec54f9890ecdf956
SoroushGReza <soroush_developer@hotmail.com>
2024-09-02 12:32:47 +0300

    - Added `useState` to manage the visibility of the offcanvas menu. - Replaced the custom `handleShow` and `handleClose` functions with `setShow` state to control the offcanvas visibility. - Updated the `Navbar.Toggle` to use the new `handleShow` method for showing the offcanvas menu. - Updated the `Navbar.Offcanvas` component to use the `show` prop and `onHide` event for controlling its visibility using state. - Applied additional styling classes (`logOutHover` and `customBurgerLogout`) to `Logout` links for better user experience.

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,17 +1,250 @@
-import React, { useRef } from "react";
+//import React, { useRef } from "react";
+//import { Navbar, Nav, Offcanvas, Container, Row, Col } from "react-bootstrap";
+//import { Link } from "react-router-dom";
+//import styles from "../styles/NavBar.module.css";
+//import logo from "../assets/images/nc-logo-black.png";
+
+//function NavBar() {
+//    const offcanvasRef = useRef(null);
+
+//    const handleClose = () => {
+//        if (offcanvasRef.current) {
+//            offcanvasRef.current.hide();
+//        }
+//    };
+
+//    // Kontrollera om användaren är inloggad
+//    const isAuthenticated = () => {
+//        const token = localStorage.getItem("access");
+//        return token !== null;
+//    };
+
+//    // Hantera utloggning
+//    const handleLogout = () => {
+//        localStorage.removeItem("access");
+//        localStorage.removeItem("refresh");
+//        window.location.reload(); // Ladda om sidan för att tillämpa ändringarna
+//    };
+
+//    return (
+//        <div className={styles.navbarContainer}>
+//            <Navbar expand="lg" className={`${styles.navbar}`}>
+//                <Container fluid>
+//                    <Row className="w-100 align-items-center">
+//                        {/* Hamburger menu toggle and logo visible on small screens */}
+//                        <Col xs={2} className="text-start d-lg-none">
+//                            <Navbar.Toggle aria-controls="offcanvasNavbar" />
+//                        </Col>
+//                        <Col xs={8} className="d-lg-none text-center">
+//                            <Link to="/" className={styles.logoLink}>
+//                                <img src={logo} alt="Logo" className={styles.logo} />
+//                            </Link>
+//                        </Col>
+
+//                        <Col xs={2} className="d-lg-none"></Col>
+
+//                        {/* Navigation Links visible on large screens */}
+//                        <Col xs={10} className="d-none d-lg-flex justify-content-start">
+//                            <Nav className="flex-grow-1 pe-3">
+//                                <Nav.Link as={Link} to="/" className={styles.logoLink}>
+//                                    <img src={logo} alt="Logo" className={styles.logo} />
+//                                </Nav.Link>
+//                                <Nav.Link as={Link} to="/" className={styles.navLink}>
+//                                    Home
+//                                </Nav.Link>
+//                                <Nav.Link as={Link} to="/services" className={styles.navLink}>
+//                                    Services
+//                                </Nav.Link>
+
+//                                {/* Appointment is always visible */}
+//                                <Nav.Link
+//                                    as={Link}
+//                                    to={isAuthenticated() ? "/book-appointment" : "/login"}
+//                                    className={styles.navLink}
+//                                >
+//                                    Appointment
+//                                </Nav.Link>
+
+//                                <Nav.Link as={Link} to="/shop" className={styles.navLink}>
+//                                    Shop
+//                                </Nav.Link>
+//                                <Nav.Link as={Link} to="/about" className={styles.navLink}>
+//                                    About Us
+//                                </Nav.Link>
+//                                <Nav.Link as={Link} to="/contact" className={styles.navLink}>
+//                                    Contact
+//                                </Nav.Link>
+
+//                                {/* Show Pre Appointment and Aftercare Tips only if authenticated */}
+//                                {isAuthenticated() && (
+//                                    <>
+//                                        <Nav.Link
+//                                            as={Link}
+//                                            to="/pre-appointment-info"
+//                                            className={styles.navLink}
+//                                        >
+//                                            Pre Appointment
+//                                        </Nav.Link>
+//                                        <Nav.Link
+//                                            as={Link}
+//                                            to="/aftercare-tips"
+//                                            className={styles.navLink}
+//                                        >
+//                                            Aftercare Tips
+//                                        </Nav.Link>
+//                                    </>
+//                                )}
+
+//                                {/* Visa Login/Register eller Logout beroende på inloggningsstatus */}
+//                                {!isAuthenticated() ? (
+//                                    <>
+//                                        <Nav.Link as={Link} to="/login" className={styles.navLink}>
+//                                            Login
+//                                        </Nav.Link>
+//                                        <Nav.Link as={Link} to="/register" className={styles.navLink}>
+//                                            Register
+//                                        </Nav.Link>
+//                                    </>
+//                                ) : (
+//                                        <Nav.Link as={Link} to="/" onClick={handleLogout} className={`${styles.logOutHover} ${styles.navLink}`}>
+//                                        Logout
+//                                    </Nav.Link>
+//                                )}
+//                            </Nav>
+//                        </Col>
+//                    </Row>
+
+//                    <Navbar.Offcanvas
+//                        id="offcanvasNavbar"
+//                        aria-labelledby="offcanvasNavbarLabel"
+//                        placement="end"
+//                        className="d-lg-none customOffcanvas"
+//                        ref={offcanvasRef}
+//                        style={{ backgroundColor: "#bbc0b5" }}
+//                    >
+//                        <Offcanvas.Header closeButton>
+//                            <Offcanvas.Title
+//                                className={styles.menuTitle}
+//                                id="offcanvasNavbarLabel"
+//                            >
+//                                Meny
+//                            </Offcanvas.Title>
+//                        </Offcanvas.Header>
+//                        <Offcanvas.Body>
+//                            <Nav className="flex-column">
+//                                <Nav.Link
+//                                    as={Link}
+//                                    to="/"
+//                                    className={styles.burgerNavLink}
+//                                    onClick={handleClose}
+//                                >
+//                                    Home
+//                                </Nav.Link>
+//                                <Nav.Link
+//                                    as={Link}
+//                                    to="/services"
+//                                    className={styles.burgerNavLink}
+//                                    onClick={handleClose}
+//                                >
+//                                    Services
+//                                </Nav.Link>
+
+//                                {/* Book Appointment is always visible */}
+//                                <Nav.Link
+//                                    as={Link}
+//                                    to={isAuthenticated() ? "/book-appointment" : "/login"}
+//                                    className={styles.burgerNavLink}
+//                                    onClick={handleClose}
+//                                >
+//                                    Book Appointment
+//                                </Nav.Link>
+
+//                                <Nav.Link
+//                                    as={Link}
+//                                    to="/shop"
+//                                    className={styles.burgerNavLink}
+//                                    onClick={handleClose}
+//                                >
+//                                    Shop Products
+//                                </Nav.Link>
+//                                <Nav.Link
+//                                    as={Link}
+//                                    to="/about"
+//                                    className={styles.burgerNavLink}
+//                                    onClick={handleClose}
+//                                >
+//                                    About Us
+//                                </Nav.Link>
+//                                <Nav.Link
+//                                    as={Link}
+//                                    to="/contact"
+//                                    className={styles.burgerNavLink}
+//                                    onClick={handleClose}
+//                                >
+//                                    Contact
+//                                </Nav.Link>
+
+//                                {/* Show Pre Appointment and Aftercare Tips only if authenticated */}
+//                                {isAuthenticated() && (
+//                                    <>
+//                                        <Nav.Link
+//                                            as={Link}
+//                                            to="/pre-appointment-info"
+//                                            className={styles.burgerNavLink}
+//                                            onClick={handleClose}
+//                                        >
+//                                            Pre Appointment
+//                                        </Nav.Link>
+//                                        <Nav.Link
+//                                            as={Link}
+//                                            to="/aftercare-tips"
+//                                            className={styles.burgerNavLink}
+//                                            onClick={handleClose}
+//                                        >
+//                                            Aftercare Tips
+//                                        </Nav.Link>
+//                                    </>
+//                                )}
+
+//                                {/* Visa Login/Register eller Logout beroende på inloggningsstatus */}
+//                                {!isAuthenticated() ? (
+//                                    <>
+//                                        <Nav.Link as={Link} to="/login" className={styles.burgerNavLink} onClick={handleClose}>
+//                                            Login
+//                                        </Nav.Link>
+//                                        <Nav.Link as={Link} to="/register" className={styles.burgerNavLink} onClick={handleClose}>
+//                                            Register
+//                                        </Nav.Link>
+//                                    </>
+//                                ) : (
+//                                        <Nav.Link as={Link} to="/" onClick={handleLogout} className={`${styles.customBurgerLogout} ${styles.burgerNavLink}`}>
+//                                        Logout
+//                                    </Nav.Link>
+//                                )}
+//                            </Nav>
+//                        </Offcanvas.Body>
+//                    </Navbar.Offcanvas>
+//                </Container>
+//            </Navbar>
+//        </div>
+//    );
+//}
+
+//export default NavBar;
+
+
+import React, { useRef, useState } from "react";
 import { Navbar, Nav, Offcanvas, Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import styles from "../styles/NavBar.module.css";
 import logo from "../assets/images/nc-logo-black.png";
 
 function NavBar() {
+    const [show, setShow] = useState(false);
     const offcanvasRef = useRef(null);
 
-    const handleClose = () => {
-        if (offcanvasRef.current) {
-            offcanvasRef.current.hide();
-        }
-    };
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     // Kontrollera om användaren är inloggad
     const isAuthenticated = () => {
@@ -33,7 +266,7 @@ function NavBar() {
                     <Row className="w-100 align-items-center">
                         {/* Hamburger menu toggle and logo visible on small screens */}
                         <Col xs={2} className="text-start d-lg-none">
-                            <Navbar.Toggle aria-controls="offcanvasNavbar" />
+                            <Navbar.Toggle aria-controls="offcanvasNavbar" onClick={handleShow} />
                         </Col>
                         <Col xs={8} className="d-lg-none text-center">
                             <Link to="/" className={styles.logoLink}>
@@ -106,7 +339,7 @@ function NavBar() {
                                         </Nav.Link>
                                     </>
                                 ) : (
-                                        <Nav.Link as={Link} to="/" onClick={handleLogout} className={styles.navLink}>
+                                    <Nav.Link as={Link} to="/" onClick={handleLogout} className={`${styles.logOutHover} ${styles.navLink}`}>
                                         Logout
                                     </Nav.Link>
                                 )}
@@ -120,13 +353,12 @@ function NavBar() {
                         placement="end"
                         className="d-lg-none customOffcanvas"
                         ref={offcanvasRef}
+                        show={show}
+                        onHide={handleClose}
                         style={{ backgroundColor: "#bbc0b5" }}
                     >
                         <Offcanvas.Header closeButton>
-                            <Offcanvas.Title
-                                className={styles.menuTitle}
-                                id="offcanvasNavbarLabel"
-                            >
+                            <Offcanvas.Title className={styles.menuTitle} id="offcanvasNavbarLabel">
                                 Meny
                             </Offcanvas.Title>
                         </Offcanvas.Header>
@@ -217,7 +449,7 @@ function NavBar() {
                                         </Nav.Link>
                                     </>
                                 ) : (
-                                    <Nav.Link as={Link} to="/" onClick={handleLogout} className={styles.burgerNavLink}>
+                                    <Nav.Link as={Link} to="/" onClick={handleLogout} className={`${styles.customBurgerLogout} ${styles.burgerNavLink}`}>
                                         Logout
                                     </Nav.Link>
                                 )}
