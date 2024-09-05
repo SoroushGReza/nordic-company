12dfa3f5d45ca0e9136797b93ae21ec1e6a811de
SoroushGReza <soroush_developer@hotmail.com>
2024-09-05 15:28:55 +0300

    - Added `TokenObtainPairView` and `TokenRefreshView` to `urlpatterns` for JWT token obtain and refresh endpoints. - Added comments to clarify the purpose of different paths in `urlpatterns`.

--- backend/urls.py
+++ backend/urls.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path("secure-admin/", admin.site.urls),
-    path("api/accounts/", include("accounts.urls")),
-    path("api/auth/", include("dj_rest_auth.urls")),
-    path("api/auth/registration/", include("dj_rest_auth.registration.urls")),
-    path('api/', include('bookings.urls')),
+    path("secure-admin/", admin.site.urls),  # Admin panelen
+    path("api/accounts/", include("accounts.urls")),  # Konto relaterade endpoints
+    path("api/auth/", include("dj_rest_auth.urls")),  # Inloggning / Logout / Token
+    path(
+        "api/auth/registration/", include("dj_rest_auth.registration.urls")
+    ),  # Registrering
+    path(
+        "api/auth/token/obtain/",
+        TokenObtainPairView.as_view(),
+        name="token_obtain_pair",
+    ),  # JWT Token obtain
+    path(
+        "api/auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"
+    ),  # JWT Token refresh
+    path("api/", include("bookings.urls")),  # Andra API endpoints
 ]
