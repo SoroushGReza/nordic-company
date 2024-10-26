f7d0fbff4e79e6b338e2d669571928242fccf169
SoroushGReza <soroush_developer@hotmail.com>
2024-10-26 15:14:12 +0300

    added static

--- backend/settings.py
+++ backend/settings.py
@@ -191,7 +191,7 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "frontend", "build"),
+    os.path.join(BASE_DIR, 'frontend', 'build', 'static'),
 ]
 
 # WHITENOISE_ROOT = BASE_DIR / "staticfiles" / "build"
