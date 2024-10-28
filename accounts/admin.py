c2621a1a283a83e3e8482fcf6bcb336e1f3c0e0a
SoroushGReza <soroush_developer@hotmail.com>
2024-10-28 13:05:03 +0300

    Enhance CustomUserAdmin with additional fields and forms:
    - Added `fieldsets` to `CustomUserAdmin` to organize user details into sections:
      - Basic information (email, password)
      - Personal info (name, surname, phone_number)
      - Permissions (is_active, is_staff, is_superuser, user_permissions)
      - Important dates (last_login, date_joined)
    
    - Introduced `add_fieldsets` in `CustomUserAdmin` to customize the user creation form:
      - Added fields for email, name, surname, phone_number, password1, password2, is_active, and is_staff to the form.

--- accounts/admin.py
+++ accounts/admin.py
@@ -10,5 +10,42 @@ class CustomUserAdmin(UserAdmin):
     search_fields = ["email", "name", "surname"]
     ordering = ["email"]
 
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("name", "surname", "phone_number")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    # Custom fieldsets for user creation form
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "name",
+                    "surname",
+                    "phone_number",
+                    "password1",
+                    "password2",
+                    "is_active",
+                    "is_staff",
+                ),
+            },
+        ),
+    )
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
