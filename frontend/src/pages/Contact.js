4efa19d3b52e194f520fd770188951d3d3451a21
SoroushGReza <soroush_developer@hotmail.com>
2024-12-01 17:42:24 +0300

    Add styling to Contact page and, palm.png to images folder, made responsive with react boorstrap

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -1,17 +1,26 @@
 import React, { useState, useEffect } from "react";
 import useAuthStatus from "../hooks/useAuthStatus";
-import "../styles/Contact.module.css";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import styles from "../styles/Contact.module.css";
+import ContactImg from "../assets/images/Contact.png";
+import PalmImg from "../assets/images/palm.png";
+import LogoBlack from "../assets/images/nc-logo-black.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 import { sendEmail } from "../utils/sendEmail";
-import { axiosReq } from "../api/axiosDefaults"; // Endast om du vill hämta användarinfo
+import { axiosReq } from "../api/axiosDefaults";
 
 const Contact = () => {
-  const { isAuthenticated } = useAuthStatus(); // Kollar inloggningsstatus
+  const { isAuthenticated } = useAuthStatus();
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     phone: "",
     message: "",
   });
+
+  const [emailSent, setEmailSent] = useState(false);
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -40,7 +49,7 @@ const Contact = () => {
     e.preventDefault();
     try {
       await sendEmail(formData);
-      alert("Your message has been sent successfully!");
+      setEmailSent(true); // Visa bekräftelsen
       setFormData({ name: "", email: "", phone: "", message: "" });
     } catch (error) {
       console.error("Error sending email:", error);
@@ -49,59 +58,115 @@ const Contact = () => {
   };
 
   return (
-    <div className="contact-page">
-      <h1>Contact</h1>
-      <form onSubmit={handleSubmit} className="contact-form">
-        <label>
-          Name (Required):
-          <input
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            required={!isAuthenticated}
-            placeholder="Your name here"
-          />
-        </label>
-        <label>
-          Email (Required):
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required={!isAuthenticated}
-            placeholder="Your email here"
-          />
-        </label>
-        <label>
-          Phone (Optional):
-          <input
-            type="tel"
-            name="phone"
-            value={formData.phone}
-            onChange={handleChange}
-            placeholder="Your phone number (optional)"
-          />
-        </label>
-        <label>
-          Message (Required):
-          <textarea
-            name="message"
-            value={formData.message}
-            onChange={handleChange}
-            required
-            placeholder="Your message here"
-          />
-        </label>
-        <button type="submit">Send</button>
-      </form>
-      <div className="contact-phone">
-        <p>Phone:</p>
-        <a href="tel:+46708234455" className="mobile-only">
-          +46 708 23 44 55
-        </a>
-      </div>
+    <div className={styles.contactPage}>
+      {/* Palm Image */}
+      <img src={PalmImg} alt="Palm Shadow" className={styles.palmImg} />
+
+      <Container fluid>
+        <Row className="justify-content-center">
+          <Col xs={12} md={10} lg={8} className="text-center">
+            <img
+              src={ContactImg}
+              alt="Contact Title"
+              className={styles.contactTitle}
+            />
+          </Col>
+        </Row>
+        {emailSent ? (
+          <Row className="justify-content-center">
+            <Col xs={12} md={8} lg={6} className="text-center">
+              <FontAwesomeIcon
+                icon={faCheckCircle}
+                className={styles.confirmationIcon}
+              />
+              <h3>Email sent successfully!</h3>
+              <p>
+                We will get back to you as soon as possible. Normally 24-48
+                hours.
+              </p>
+            </Col>
+          </Row>
+        ) : (
+          <Row className="justify-content-center">
+            <Col xs={12} md={8} lg={6} className={styles.contactFormContainer}>
+              <form onSubmit={handleSubmit} className={styles.contactForm}>
+                <h2>Email</h2>
+                <label>
+                  Name:
+                  <input
+                    type="text"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    required={!isAuthenticated}
+                    placeholder="Your name here"
+                  />
+                </label>
+                <label>
+                  Email:
+                  <input
+                    type="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    required={!isAuthenticated}
+                    placeholder="Your email here"
+                  />
+                </label>
+                <label>
+                  Phone (Optional):
+                  <input
+                    type="tel"
+                    name="phone"
+                    value={formData.phone}
+                    onChange={handleChange}
+                    placeholder="Your phone number (optional)"
+                  />
+                </label>
+                <label>
+                  Message:
+                  <textarea
+                    name="message"
+                    value={formData.message}
+                    onChange={handleChange}
+                    required
+                    placeholder="Your message here"
+                  />
+                </label>
+                <Button type="submit" className={styles.sendButton}>
+                  Send
+                </Button>
+              </form>
+            </Col>
+          </Row>
+        )}
+
+        <Row className="justify-content-center contact-footer">
+          <Col xs={12} md={6} lg={4} className="text-center">
+            <img
+              src={LogoBlack}
+              alt="Nordic Company"
+              className={styles.contactLogo}
+            />
+            <div className={styles.contactFormContainer}>
+              <h4>Phone</h4>
+              <a href="tel:+46708234455" className={styles.contactPhone}>
+                +46 708 23 44 55
+              </a>
+              <p className={styles.whatsappText}>Available on WhatsApp</p>
+            </div>
+          </Col>
+          {/* Instagram Link */}
+          <a
+            href="https://www.instagram.com/facebykristine/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`${styles["link"]} text-center mt-4`}
+          >
+            <FontAwesomeIcon className={styles.instagram} icon={faInstagram} />
+          </a>
+        </Row>
+      </Container>
     </div>
   );
 };
