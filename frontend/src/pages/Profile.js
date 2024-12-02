c9497e2e83e05e97f9480c93d0f0edcccfe8593e
SoroushGReza <soroush_developer@hotmail.com>
2024-12-02 17:22:20 +0300

    Enhanced Profile Page Styling and Structure:
    Profile.js:
    - Imported `Profile.module.css` for improved styling and layout consistency.
    - Wrapped the main container with a `profilePage` class for structured styling.
    - Integrated a decorative profile text image using `ProfileImageText` for visual enhancement.
    - Refactor import statements for better organization
    
    Profile.module.css:
    - Created `.profilePage` class for consistent padding and font styling across page.
    - Added `.profileImageText` styles for responsive display of the decorative profile image.
    - Introducee media queries for seamless adaptation to different screen sizes.

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,4 +1,10 @@
 import React, { useState, useEffect } from "react";
+// API
+import { axiosReq } from "../api/axiosDefaults";
+// Hooks
+import useAuthStatus from "../hooks/useAuthStatus";
+// Styling & Images
+import styles from "../styles/Profile.module.css";
 import {
   Form,
   Button,
@@ -9,8 +15,7 @@ import {
   Modal,
   Alert,
 } from "react-bootstrap";
-import { axiosReq } from "../api/axiosDefaults";
-import useAuthStatus from "../hooks/useAuthStatus";
+import ProfileImageText from "../assets/images/Profile.png";
 
 function Profile() {
   useAuthStatus();
@@ -147,162 +152,173 @@ function Profile() {
   }
 
   return (
-    <Container className="mt-5">
-      <h1>Profile</h1>
-      <Row>
-        <Col md={6}>
-          {/* Display any errors */}
-          {errors.non_field_errors && (
-            <Alert variant="danger">{errors.non_field_errors}</Alert>
-          )}
-          {/* Profile Update Form */}
-          <Form onSubmit={handleProfileUpdate}>
-            <Form.Group controlId="profile_image">
-              {imagePreview && (
-                <Image
-                  src={imagePreview}
-                  roundedCircle
-                  width={100}
-                  height={100}
-                  className="mt-2 mb-2"
-                />
-              )}
-              {errors.profile_image && (
-                <div className="text-danger">{errors.profile_image}</div>
-              )}
-              <Form.Control
-                type="file"
-                name="profile_image"
-                accept="image/*"
-                onChange={handleImageChange}
-              />
-            </Form.Group>
-            <Form.Group controlId="name">
-              <Form.Label>Name</Form.Label>
-              <Form.Control
-                type="text"
-                name="name"
-                value={profileData.name || ""}
-                onChange={handleProfileChange}
-              />
-              {errors.name && <div className="text-danger">{errors.name}</div>}
-            </Form.Group>
-            <Form.Group controlId="surname">
-              <Form.Label>Surname</Form.Label>
-              <Form.Control
-                type="text"
-                name="surname"
-                value={profileData.surname || ""}
-                onChange={handleProfileChange}
-              />
-              {errors.surname && (
-                <div className="text-danger">{errors.surname}</div>
-              )}
-            </Form.Group>
-            <Form.Group controlId="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control
-                type="email"
-                name="email"
-                value={profileData.email || ""}
-                onChange={handleProfileChange}
-              />
-              {errors.email && (
-                <div className="text-danger">{errors.email}</div>
-              )}
-            </Form.Group>
-            <Form.Group controlId="phone_number">
-              <Form.Label>Phone Number</Form.Label>
-              <Form.Control
-                type="text"
-                name="phone_number"
-                value={profileData.phone_number || ""}
-                onChange={handleProfileChange}
-              />
-              {errors.phone_number && (
-                <div className="text-danger">{errors.phone_number}</div>
-              )}
-            </Form.Group>
-            <Button variant="primary" type="submit" className="mt-3">
-              Update Profile
-            </Button>
-          </Form>
-        </Col>
-        <Col md={6}>
-          {/* Change Password Form */}
-          <h3 className="mt-4">Change Password</h3>
-          {errors.old_password && (
-            <Alert variant="danger">{errors.old_password}</Alert>
-          )}
-          <Form onSubmit={handleChangePassword}>
-            <Form.Group controlId="old_password">
-              <Form.Label>Old Password</Form.Label>
-              <Form.Control
-                type="password"
-                name="old_password"
-                value={passwordData.old_password}
-                onChange={handlePasswordChange}
-              />
-            </Form.Group>
-            <Form.Group controlId="new_password">
-              <Form.Label>New Password</Form.Label>
-              <Form.Control
-                type="password"
-                name="new_password"
-                value={passwordData.new_password}
-                onChange={handlePasswordChange}
-              />
-            </Form.Group>
-            <Button variant="primary" type="submit" className="mt-3">
-              Change Password
-            </Button>
-          </Form>
-          {/* Delete Account Button */}
-          <h3 className="mt-4 text-danger">Delete Account</h3>
-          <Button
-            variant="danger"
-            onClick={() => setShowDeleteModal(true)}
-            className="mt-2"
-          >
-            Delete Account
-          </Button>
-        </Col>
-      </Row>
-      {/* Modal for Account Deletion Confirmation */}
-      <Modal show={showDeleteModal} onHide={() => setShowDeleteModal(false)}>
-        <Modal.Header closeButton>
-          <Modal.Title>Confirm Account Deletion</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form onSubmit={handleDeleteAccount}>
-            {errors.password && (
-              <Alert variant="danger">{errors.password}</Alert>
+    <div className={styles.profilePage}>
+      <Container fluid className="mt-5">
+        {/* Profile Image Text */}
+        <Row className="justify-content-center">
+        <img
+          src={ProfileImageText}
+          alt="Profile"
+          className={styles.profileImageText}
+        />
+        </Row>
+        <Row>
+          <Col md={6}>
+            {/* Display any errors */}
+            {errors.non_field_errors && (
+              <Alert variant="danger">{errors.non_field_errors}</Alert>
             )}
-            <Form.Group controlId="delete_password">
-              <Form.Label>Enter your password to confirm</Form.Label>
-              <Form.Control
-                type="password"
-                name="password"
-                value={deletePassword}
-                onChange={(e) => setDeletePassword(e.target.value)}
-              />
-            </Form.Group>
-            <div className="mt-3">
-              <Button variant="danger" type="submit">
-                Delete Account
+            {/* Profile Update Form */}
+            <Form onSubmit={handleProfileUpdate}>
+              <Form.Group controlId="profile_image">
+                {imagePreview && (
+                  <Image
+                    src={imagePreview}
+                    roundedCircle
+                    width={100}
+                    height={100}
+                    className="mt-2 mb-2"
+                  />
+                )}
+                {errors.profile_image && (
+                  <div className="text-danger">{errors.profile_image}</div>
+                )}
+                <Form.Control
+                  type="file"
+                  name="profile_image"
+                  accept="image/*"
+                  onChange={handleImageChange}
+                />
+              </Form.Group>
+              <Form.Group controlId="name">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="name"
+                  value={profileData.name || ""}
+                  onChange={handleProfileChange}
+                />
+                {errors.name && (
+                  <div className="text-danger">{errors.name}</div>
+                )}
+              </Form.Group>
+              <Form.Group controlId="surname">
+                <Form.Label>Surname</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="surname"
+                  value={profileData.surname || ""}
+                  onChange={handleProfileChange}
+                />
+                {errors.surname && (
+                  <div className="text-danger">{errors.surname}</div>
+                )}
+              </Form.Group>
+              <Form.Group controlId="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  name="email"
+                  value={profileData.email || ""}
+                  onChange={handleProfileChange}
+                />
+                {errors.email && (
+                  <div className="text-danger">{errors.email}</div>
+                )}
+              </Form.Group>
+              <Form.Group controlId="phone_number">
+                <Form.Label>Phone Number</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="phone_number"
+                  value={profileData.phone_number || ""}
+                  onChange={handleProfileChange}
+                />
+                {errors.phone_number && (
+                  <div className="text-danger">{errors.phone_number}</div>
+                )}
+              </Form.Group>
+              <Button variant="primary" type="submit" className="mt-3">
+                Update Profile
               </Button>
-              <Button
-                variant="secondary"
-                onClick={() => setShowDeleteModal(false)}
-                className="ml-2"
-              >
-                Cancel
+            </Form>
+          </Col>
+          <Col md={6}>
+            {/* Change Password Form */}
+            <h3 className="mt-4">Change Password</h3>
+            {errors.old_password && (
+              <Alert variant="danger">{errors.old_password}</Alert>
+            )}
+            <Form onSubmit={handleChangePassword}>
+              <Form.Group controlId="old_password">
+                <Form.Label>Old Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  name="old_password"
+                  value={passwordData.old_password}
+                  onChange={handlePasswordChange}
+                />
+              </Form.Group>
+              <Form.Group controlId="new_password">
+                <Form.Label>New Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  name="new_password"
+                  value={passwordData.new_password}
+                  onChange={handlePasswordChange}
+                />
+              </Form.Group>
+              <Button variant="primary" type="submit" className="mt-3">
+                Change Password
               </Button>
-            </div>
-          </Form>
-        </Modal.Body>
-      </Modal>
-    </Container>
+            </Form>
+            {/* Delete Account Button */}
+            <h3 className="mt-4 text-danger">Delete Account</h3>
+            <Button
+              variant="danger"
+              onClick={() => setShowDeleteModal(true)}
+              className="mt-2"
+            >
+              Delete Account
+            </Button>
+          </Col>
+        </Row>
+        {/* Modal for Account Deletion Confirmation */}
+        <Modal show={showDeleteModal} onHide={() => setShowDeleteModal(false)}>
+          <Modal.Header closeButton>
+            <Modal.Title>Confirm Account Deletion</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form onSubmit={handleDeleteAccount}>
+              {errors.password && (
+                <Alert variant="danger">{errors.password}</Alert>
+              )}
+              <Form.Group controlId="delete_password">
+                <Form.Label>Enter your password to confirm</Form.Label>
+                <Form.Control
+                  type="password"
+                  name="password"
+                  value={deletePassword}
+                  onChange={(e) => setDeletePassword(e.target.value)}
+                />
+              </Form.Group>
+              <div className="mt-3">
+                <Button variant="danger" type="submit">
+                  Delete Account
+                </Button>
+                <Button
+                  variant="secondary"
+                  onClick={() => setShowDeleteModal(false)}
+                  className="ml-2"
+                >
+                  Cancel
+                </Button>
+              </div>
+            </Form>
+          </Modal.Body>
+        </Modal>
+      </Container>
+    </div>
   );
 }
 
